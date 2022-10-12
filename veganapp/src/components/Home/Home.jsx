e1eba2f677d88980e37229a04711f0768505728d
Blito13 <lesta4444@hotmail.com>
2022-10-11 21:04:42 -0300

    styles

--- veganapp/src/components/Home/Home.jsx
+++ veganapp/src/components/Home/Home.jsx
@@ -1,8 +1,33 @@
 import React from "react";
-
+import bagels from "../../media/baggles.jpg" 
+import styles from "./Home.module.css"
+import About from "../About/AboutUs";
 const Home = () => {
     return (
-        <h1>Soy Home</h1>
+        <div>
+             <About></About>
+    <section  >
+        {/* a esto hay que hacer con  cartas y map */}
+        <div className={styles.container}>
+         <h2>Algunos de Nuestros Productos</h2>
+            <h3>Panes</h3>
+            <div className={styles.card}>
+                <p className="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita corrupti corporis labore debitis? Dolorem cupiditate aspernatur quidem excepturi hic perferendis aliquid accusamus non, iste odit dicta tempore asperiores ipsam quod!</p>
+            </div>
+            <h3>Brownies</h3>
+            <div className={styles.card}>
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam incidunt dignissimos velit aspernatur asperiores distinctio. Necessitatibus porro, cum nam earum explicabo excepturi vero facilis quibusdam repellat, dolorum reprehenderit odio quisquam.</p>
+            </div>
+            <h3>Baggles</h3>
+            <div className={styles.card}>
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis sapiente aperiam esse, deserunt blanditiis similique temporibus nulla rem ab at nisi quisquam distinctio iusto id ea animi enim consequuntur recusandae!</p>
+            </div>
+        
+        </div>
+
+    </section>
+    {/* Aca se debe renderizar el componente Contact */}
+        </div>
     )
 }
 export default Home;
--- veganapp/src/components/NavBar/NavBar.jsx
+++ veganapp/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import styles from "./NavBar.module.css"
 
 const NavBar = () => {
     return(
-        <h1>hola soy navbar</h1>
+        <div className={styles.container}>
+        <p className={styles.logo}>Amaso Criando{/* <br> */}Pasteleria Integral Vegana</p>
+        <nav className={styles.nav}>
+            <a /* href="index.html" */>Inicio</a>
+            <a href="#hero">Quienes Somos?</a>
+            <a /* href="#prod" */>Nuestros Productos</a>
+            <a /* href="#contact" */>Contactanos</a>
+        </nav>
+    </div>
     )
 }
 export default NavBar
