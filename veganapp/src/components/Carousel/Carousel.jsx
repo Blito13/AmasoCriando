68f6eef15484e12cad561164a12237ce8d649a4a
blito13 <blito13@github.com>
2023-09-03 23:15:15 -0300

    ColorsHero

--- veganapp/src/components/Carousel/Carousel.jsx
+++ veganapp/src/components/Carousel/Carousel.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useState  , useEffect} from "react";
 import styles from "./Carousel.module.css"
 import { productos } from "../../utils/utils";
-
 const Carousel = ()=>{
+  const [animation , setAnimation] = useState("");
+  const [index , setIndex] = useState("");
+
     return(
            <div className={styles.container}>
-             <span className={styles.move}>
-                {
-                    productos.map(e => 
-                        <img src={e.imgn} className={styles.image}></img>
-                        )
-                }
-             </span>
+            <div className={styles.prueva}>
+             <div className={styles.move}>
+                <h1>100% vegan bakery</h1>
+                <p>
+                  Nuestros productos estan realizados con harina integral organica , azucar mascabo y leches vegetales ,
+                  en su mayoria elaborados con productos sin refinar y 100% veganos.
+                </p>
+             </div>
+             {productos.map( e => 
+              <img className={styles.image} /* style ={{transition :"2s" ,left: "1px"}}  */src={e.imgn} alt="" />
+             )}
+            </div>
             </div>
     )
 }
