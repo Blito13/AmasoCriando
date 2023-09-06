6115ec04c4441a3547bf0a7cf2fc3ed60720a3b3
blito13 <blito13@github.com>
2023-09-06 15:18:42 -0300

    plus

--- veganapp/src/components/Carousel/Carousel.jsx
+++ veganapp/src/components/Carousel/Carousel.jsx
@@ -8,16 +8,18 @@ const Carousel = ()=>{
     return(
            <div className={styles.container}>
             <div className={styles.prueva}>
-             <div className={styles.move}>
+             <div className={styles.static}>
                 <h1>100% vegan bakery</h1>
                 <p>
                   Nuestros productos estan realizados con harina integral organica , azucar mascabo y leches vegetales ,
                   en su mayoria elaborados con productos sin refinar y 100% veganos.
                 </p>
              </div>
+             <div className={styles.move}>
              {productos.map( e => 
               <img className={styles.image} /* style ={{transition :"2s" ,left: "1px"}}  */src={e.imgn} alt="" />
              )}
+             </div>
             </div>
             </div>
     )
--- veganapp/src/components/Home/Home.jsx
+++ veganapp/src/components/Home/Home.jsx
@@ -10,32 +10,43 @@ import { mock , productos } from "../../utils/utils";
 const Home = () => {
 const [from ,setFrom ] = useState(0);
 const [to , setTo] = useState(3);
+const [opac ,setOpac] =useState("");
 
 const handleNext = () =>{
     if(to >= 7) return;
-    setFrom(to);
-    setTo(to + 3);
+    setOpac({transition : "2s" ,opacity: "0.0" });
+    setTimeout(()=>{
+        setFrom(to);
+        setTo(to + 3);
+        setOpac({transition : "1s" ,opacity: "1" });
+    }, 1500)
+;
 };
 const handlePrev = () =>{
     if (from === 0) return ;
+    setOpac({transition : "2s" ,opacity: "0.0" });
+    setTimeout(()=>{
         setFrom(from - 3);
-        setTo(to - 3)
+        setTo(to - 3);
+        setOpac({transition : "1s" ,opacity: "1" });
+    },1500);
 };
     return (
         <div className={styles.container}>   
-           <div className={styles.content}>
-           
                 <Carousel></Carousel>
-           
-            
-           <div className = {styles.cards}>
-            <button onClick={handlePrev}> Prev</button>
-            {
-            mock.slice(from,to).map(e =>
-                <Card imgn ={e.imgn} texto = {e.texto} id = {e.id} name = {e.name}></Card>
+           <div className={styles.content}>
+            <button onClick={handlePrev}>Prev</button>
+           <div className = {styles.cards} style={opac? opac : null}>
+             {mock.slice(from,to).map(e =>
+                <Card 
+                 imgn ={e.imgn}
+                 texto = {e.texto}
+                 id = {e.id}
+                 name = {e.name}>
+                </Card>
                 )}
-                <button onClick={handleNext}>Next</button>
            </div>
+            <button onClick={handleNext}>Next</button>
            </div>
           
             
